d9f4dcd4c821aee46c279291477eb47c79f6f153
Marco Antônio Furtado <89411657+marcofurtadodev@users.noreply.github.com>
2023-09-03 18:34:36 +0000

    configurando e aplicando identação de código

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
-export default function Home(){
-    return <h1>You are gonna be proud!</h1>;
+export default function Home() {
+  return <h1>You are gonna be proud!</h1>;
 }
